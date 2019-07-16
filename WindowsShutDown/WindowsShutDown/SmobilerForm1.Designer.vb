ab3f5919c0a8fef5448fb18f9c68bd1422b27c86
xyhuai <139069910@qq.com>
2019-07-16 13:07:58 +0800

    1

--- WindowsShutDown/WindowsShutDown/SmobilerForm1.Designer.vb
+++ WindowsShutDown/WindowsShutDown/SmobilerForm1.Designer.vb
@@ -16,22 +16,172 @@ Partial Public Class SmobilerForm1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
         Me.Button1 = New Smobiler.Core.Controls.Button()
+        Me.Line1 = New Smobiler.Core.Controls.Line()
+        Me.Label1 = New Smobiler.Core.Controls.Label()
+        Me.Button2 = New Smobiler.Core.Controls.Button()
+        Me.Button3 = New Smobiler.Core.Controls.Button()
+        Me.Button4 = New Smobiler.Core.Controls.Button()
+        Me.Button5 = New Smobiler.Core.Controls.Button()
+        Me.Line2 = New Smobiler.Core.Controls.Line()
+        Me.Label2 = New Smobiler.Core.Controls.Label()
+        Me.Button6 = New Smobiler.Core.Controls.Button()
+        Me.Button7 = New Smobiler.Core.Controls.Button()
+        Me.Button8 = New Smobiler.Core.Controls.Button()
+        Me.Button9 = New Smobiler.Core.Controls.Button()
+        Me.txtPos = New Smobiler.Core.Controls.TextBox()
+        Me.Line3 = New Smobiler.Core.Controls.Line()
+        Me.Button10 = New Smobiler.Core.Controls.Button()
+        Me.Button11 = New Smobiler.Core.Controls.Button()
+        Me.Label3 = New Smobiler.Core.Controls.Label()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(96, 154)
+        Me.Button1.Location = New System.Drawing.Point(95, 9)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(100, 30)
         Me.Button1.Text = "立即关机"
         '
+        'Line1
+        '
+        Me.Line1.Location = New System.Drawing.Point(0, 54)
+        Me.Line1.Name = "Line1"
+        Me.Line1.Size = New System.Drawing.Size(300, 2)
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(0, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 19)
+        Me.Label1.Text = "鼠标移动"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(95, 86)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 30)
+        Me.Button2.Text = "上"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(0, 125)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 30)
+        Me.Button3.Text = "左"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(207, 125)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 30)
+        Me.Button4.Text = "右"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(95, 166)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 30)
+        Me.Button5.Text = "下"
+        '
+        'Line2
+        '
+        Me.Line2.Location = New System.Drawing.Point(0, 204)
+        Me.Line2.Name = "Line2"
+        Me.Line2.Size = New System.Drawing.Size(300, 2)
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(0, 206)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 19)
+        Me.Label2.Text = "键盘方向"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(95, 235)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 30)
+        Me.Button6.Text = "上"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(0, 274)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 30)
+        Me.Button7.Text = "左"
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(207, 274)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 30)
+        Me.Button8.Text = "右"
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(95, 315)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 30)
+        Me.Button9.Text = "下"
+        '
+        'txtPos
+        '
+        Me.txtPos.Location = New System.Drawing.Point(195, 56)
+        Me.txtPos.Name = "txtPos"
+        Me.txtPos.Size = New System.Drawing.Size(100, 24)
+        Me.txtPos.Text = "10"
+        '
+        'Line3
+        '
+        Me.Line3.Location = New System.Drawing.Point(0, 358)
+        Me.Line3.Name = "Line3"
+        Me.Line3.Size = New System.Drawing.Size(300, 2)
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(0, 398)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(100, 30)
+        Me.Button10.Text = "左键单击"
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(207, 398)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(100, 30)
+        Me.Button11.Text = "右键单击"
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(135, 60)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 17)
+        Me.Label3.Text = "移动像素:"
+        '
         'SmobilerForm1
         '
-        Me.Controls.AddRange(New Smobiler.Core.Controls.MobileControl() {Me.Button1})
+        Me.Controls.AddRange(New Smobiler.Core.Controls.MobileControl() {Me.Button1, Me.Line1, Me.Label1, Me.Button2, Me.Button3, Me.Button4, Me.Button5, Me.Line2, Me.Label2, Me.Button6, Me.Button7, Me.Button8, Me.Button9, Me.txtPos, Me.Line3, Me.Button10, Me.Button11, Me.Label3})
         Me.Name = "SmobilerForm1"
 
     End Sub
 
     Friend WithEvents Button1 As Controls.Button
+    Friend WithEvents Line1 As Controls.Line
+    Friend WithEvents Label1 As Controls.Label
+    Friend WithEvents Button2 As Controls.Button
+    Friend WithEvents Button3 As Controls.Button
+    Friend WithEvents Button4 As Controls.Button
+    Friend WithEvents Button5 As Controls.Button
+    Friend WithEvents Line2 As Controls.Line
+    Friend WithEvents Label2 As Controls.Label
+    Friend WithEvents Button6 As Controls.Button
+    Friend WithEvents Button7 As Controls.Button
+    Friend WithEvents Button8 As Controls.Button
+    Friend WithEvents Button9 As Controls.Button
+    Friend WithEvents txtPos As Controls.TextBox
+    Friend WithEvents Line3 As Controls.Line
+    Friend WithEvents Button10 As Controls.Button
+    Friend WithEvents Button11 As Controls.Button
+    Friend WithEvents Label3 As Controls.Label
 
 #End Region
 
--- WindowsShutDown/WindowsShutDown/SmobilerForm1.vb
+++ WindowsShutDown/WindowsShutDown/SmobilerForm1.vb
@@ -16,11 +16,176 @@ Public Class SmobilerForm1
         InitializeComponent()
 
     End Sub
+    ''' <summary>
+    ''' 注册模拟鼠标移动方法
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="y"></param>
+    Private Declare Sub SetCursorPos Lib "user32" (ByVal x As Integer, ByVal y As Integer)
+    ''' <summary>
+    ''' 注册模拟鼠标按键方法
+    ''' </summary>
+    ''' <param name="dwFlags"></param>
+    ''' <param name="dx"></param>
+    ''' <param name="dy"></param>
+    ''' <param name="cButtons"></param>
+    ''' <param name="dwExtraInfo"></param>
+    Private Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+    Private Const MOUSEEVENTF_LEFTDOWN = &H2 '鼠标左键按下
+    Private Const MOUSEEVENTF_LEFTUP = &H4 '鼠标左键抬起
+    Private Const MOUSEEVENTF_RIGHTDOWN = &H8 '鼠标右键按下
+    Private Const MOUSEEVENTF_RIGHTUP = &H10 '鼠标右键抬起
+    ''' <summary>
+    ''' 注册获取当前鼠标坐标方法
+    ''' </summary>
+    ''' <param name="lpPoint"></param>
+    Private Declare Sub GetCursorPos Lib "user32" (lpPoint As Integer())
 
+    '''' <summary>
+    '''' 注册模拟按键方法
+    '''' </summary>
+    '''' <param name="wMsg"></param>
+    '''' <param name="wParam"></param>
+    '''' <returns></returns>
+    'Private Declare Function SendMessage Lib "user32" Alias "SendMessageA" (ByVal hwnd As Integer, ByVal wMsg As Long, ByVal wParam As Long) As Long
+    'Private Const WM_KEYDOWN = &H100 '按下
+    'Private Const WM_KEYUP = &H101 '释放
+
+    ''' <summary>
+    ''' 注册模拟按键方法
+    ''' </summary>
+    ''' <param name="bVk"></param>
+    ''' <param name="Scan"></param>
+    ''' <param name="dwFlags"></param>
+    ''' <param name="dwExtraInfo"></param>
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal Scan As Byte, ByVal dwFlags As Integer, ByVal dwExtraInfo As Integer)
+    Const KEYEVENTF_KEYUP = &H2 '释放按键常数
+    Private Const Key_UP = 38   '上
+    Private Const Key_DOWN = 40   '下
+    Private Const Key_LEFT = 37   '左
+    Private Const Key_RIGHT = 39   '右
+
+    ''' <summary>
+    ''' 点击一次右键
+    ''' </summary>
+    Private Sub rightChick()
+        mouse_event（MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0）
+        mouse_event（MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0）
+    End Sub
+    ''' <summary>
+    ''' 点击一次左键
+    ''' </summary>
+    Private Sub lefghtChick()
+        mouse_event（MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0）
+        mouse_event（MOUSEEVENTF_LEFTUP, 0, 0, 0, 0）
+    End Sub
+    ''' <summary>
+    ''' 获取当前鼠标位置
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function getNowPos() As Integer()
+        Dim pos(2) As Integer
+        GetCursorPos(pos)
+        Return pos
+    End Function
+
+    ''' <summary>
+    ''' 立即关机
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub Button1_Press(sender As Object, e As EventArgs) Handles Button1.Press
 
         System.Diagnostics.Process.Start("shutdown.exe", "-s -t 0")
 
     End Sub
+    ''' <summary>
+    ''' 鼠标上移
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button2_Press(sender As Object, e As EventArgs) Handles Button2.Press
+        Dim pos() As Integer = getNowPos()
+        Call SetCursorPos(pos(0), pos(1) - CInt(txtPos.Text))
 
+    End Sub
+    ''' <summary>
+    ''' 鼠标左移
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button3_Press(sender As Object, e As EventArgs) Handles Button3.Press
+        Dim pos() As Integer = getNowPos()
+        Call SetCursorPos(pos(0) - CInt(txtPos.Text), pos(1))
+    End Sub
+    ''' <summary>
+    ''' 鼠标右移
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button4_Press(sender As Object, e As EventArgs) Handles Button4.Press
+        Dim pos() As Integer = getNowPos()
+        Call SetCursorPos(pos(0) + CInt(txtPos.Text), pos(1))
+    End Sub
+    ''' <summary>
+    ''' 鼠标下移
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button5_Press(sender As Object, e As EventArgs) Handles Button5.Press
+        Dim pos() As Integer = getNowPos()
+        Call SetCursorPos(pos(0), pos(1) + CInt(txtPos.Text))
+    End Sub
+    ''' <summary>
+    ''' 键盘上
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button6_Press(sender As Object, e As EventArgs) Handles Button6.Press
+        Call keybd_event(Key_UP, 0, 0, 0) '按下
+        Call keybd_event(Key_UP, 0, KEYEVENTF_KEYUP, 0) '释放
+    End Sub
+    ''' <summary>
+    ''' 键盘左
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button7_Press(sender As Object, e As EventArgs) Handles Button7.Press
+        Call keybd_event(Key_LEFT, 0, 0, 0) '按下
+        Call keybd_event(Key_LEFT, 0, KEYEVENTF_KEYUP, 0) '释放
+    End Sub
+    ''' <summary>
+    ''' 键盘右
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button8_Press(sender As Object, e As EventArgs) Handles Button8.Press
+        Call keybd_event(Key_RIGHT, 0, 0, 0) '按下
+        Call keybd_event(Key_RIGHT, 0, KEYEVENTF_KEYUP, 0) '释放
+    End Sub
+    ''' <summary>
+    ''' 键盘下
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button9_Press(sender As Object, e As EventArgs) Handles Button9.Press
+        Call keybd_event(Key_DOWN, 0, 0, 0) '按下
+        Call keybd_event(Key_DOWN, 0, KEYEVENTF_KEYUP, 0) '释放
+    End Sub
+    ''' <summary>
+    ''' 左键单击
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button10_Press(sender As Object, e As EventArgs) Handles Button10.Press
+        lefghtChick()
+    End Sub
+    ''' <summary>
+    ''' 右键单击
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button11_Press(sender As Object, e As EventArgs) Handles Button11.Press
+        rightChick()
+    End Sub
 End Class
